6ccd5478ad250a541b6dc480b93dc6869551be3a
sruck007 <121472649+sruck007@users.noreply.github.com>
2025-02-10 10:32:57 -0800

    SQL-Queries.sql

--- SQL-Queries.sql
+++ SQL-Queries.sql
@@ -1,5 +1,5 @@
-1️⃣ Insurance Mix Reimbursement (Pie Chart)
-## This query groups insurance payers and calculates total reimbursements. This is useful for understanding payer distribution and identifying revenue sources
+1. Insurance Mix Reimbursement (Pie Chart)
+** This query groups insurance payers and calculates total reimbursements. This is useful for understanding payer distribution and identifying revenue sources
 
 SELECT 
     PayerType, 
@@ -9,8 +9,9 @@ GROUP BY PayerType
 ORDER BY TotalReimbursement DESC;
 
 
-2️⃣ Yearly Insurance Reimbursement Trends (Line Chart)
-##This query breaks down yearly reimbursements by month and payer type, helping EMS leadership track financial trends and seasonal variations.
+2. Yearly Insurance Reimbursement Trends (Line Chart)
+**This query breaks down yearly reimbursements by month and payer type, helping EMS leadership track financial trends and seasonal variations.
+
 SELECT 
     YEAR(DOS) AS ServiceYear,
     MONTH(DOS) AS ServiceMonth,
@@ -20,8 +21,10 @@ FROM Payer_Reimbursement_Data
 GROUP BY YEAR(DOS), MONTH(DOS), PayerType
 ORDER BY ServiceYear, ServiceMonth;
 
-3️⃣ Level of Service Analysis (Heat Map)
-##This query connects transport type, EMS service levels, and patient conditions, helping identify which conditions require higher-level EMS responses
+️3. Level of Service Analysis (Heat Map)
+
+**This query connects transport type, EMS service levels, and patient conditions, helping identify which conditions require higher-level EMS responses
+
 SELECT 
     e.TransportType,
     e.LevelOfService,
@@ -32,7 +35,7 @@ JOIN APC_DX_Mapping adm ON e.ProcedureID = adm.ProcedureID
 GROUP BY e.TransportType, e.LevelOfService, adm.ICD10_Description
 ORDER BY IncidentCount DESC;
 
-4️⃣ EMS Response vs. Transport Time (Bar Chart)
+️4. EMS Response vs. Transport Time (Bar Chart)
 SELECT 
     Zone, 
     AVG(DATEDIFF(MINUTE, CallReceivedTime, OnSceneTime)) AS AvgResponseTime,
@@ -41,8 +44,8 @@ FROM EMS_Incident_Data
 GROUP BY Zone
 ORDER BY AvgTransportTime DESC;
 
-5️⃣ Incident Location Response (Mapping)
-##"This query extracts latitude and longitude from EMS reports, allowing us to visualize incident hotspots and response distribution across the city
+5. Incident Location Response (Mapping)
+**"This query extracts latitude and longitude from EMS reports, allowing us to visualize incident hotspots and response distribution across the city
 SELECT 
     IncidentID, 
     Zone,
